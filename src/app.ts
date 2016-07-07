6873defbd5956765b192d12362a07143a5b06155
Alexander Tatchin <alexander.tatchin@gmail.com>
2016-07-07 14:29:55 +0300

    boiler debuging

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,4 @@
-window.alert('Typescript here..')
+import * as express from 'express';
 
+var app = express().listen(3000, () => console.log('UP on 3000'));
+app.getConnections((err: Error, count: number) => console.log(err || count));
